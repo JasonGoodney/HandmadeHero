031ddf806df13d57110d55759292c2d2a946e1b0
jason goodney <jason.goodney@gmail.com>
2025-04-27 22:24:53 -0400

    day 003

--- handmade/code/macos_main.mm
+++ handmade/code/macos_main.mm
@@ -1,15 +1,23 @@
 #include <AppKit/AppKit.h>
+#include <Foundation/Foundation.h>
+#include <cstdint>
+#include <cstdlib>
 #include <stdio.h>
 
-static int GlobalRenderingWidth = 1280;
-static int GlobalRenderingHeight = 720;
+#define internal static
+#define local_persist static
+#define global_variable static
 
-static BOOL Running = YES;
+global_variable int GlobalRenderingWidth = 1280;
+global_variable int GlobalRenderingHeight = 720;
+global_variable BOOL Running;
 
-@interface HandmadeWindowDelegate: NSObject<NSWindowDelegate>;
+@interface HandmadeWindowDelegate : NSObject <NSWindowDelegate>
+;
 @end
 
-@implementation HandmadeWindowDelegate;
+@implementation HandmadeWindowDelegate
+;
 - (void)windowWillClose:(NSNotification *)notification {
   Running = NO;
 }
@@ -31,24 +39,55 @@ int main(int argc, const char *argv[]) {
                           NSWindowStyleMaskResizable
                   backing:NSBackingStoreBuffered
                     defer:NO];
-  [window setBackgroundColor:NSColor.redColor];
+  [window setBackgroundColor:NSColor.blackColor];
   [window setTitle:@"Handmade Hero"];
   [window makeKeyAndOrderFront:nil];
 
-  HandmadeWindowDelegate *windowDelegate = [[HandmadeWindowDelegate alloc] init];
+  HandmadeWindowDelegate *windowDelegate =
+      [[HandmadeWindowDelegate alloc] init];
   [window setDelegate:windowDelegate];
 
+  // pitch
+  int bitmapWidth = window.contentView.bounds.size.width;
+  int bitmapHeight = window.contentView.bounds.size.height;
+  int bytesPerPixel = 4;
+
+  int pitch = bitmapWidth * bytesPerPixel;
+
+  uint8_t *buffer = (uint8_t *)malloc(pitch * bitmapHeight);
+
+  Running = true;
   while (Running) {
+
+    @autoreleasepool {
+      NSBitmapImageRep *imageRep = [[[NSBitmapImageRep alloc]
+          initWithBitmapDataPlanes:&buffer
+                        pixelsWide:bitmapWidth
+                        pixelsHigh:bitmapHeight
+                     bitsPerSample:8
+                   samplesPerPixel:bytesPerPixel
+                          hasAlpha:YES
+                          isPlanar:NO
+                    colorSpaceName:NSDeviceRGBColorSpace
+                       bytesPerRow:pitch
+                      bitsPerPixel:32] autorelease];
+      NSSize imageSize = NSMakeSize(bitmapWidth, bitmapHeight);
+      NSImage *image = [[[NSImage alloc] initWithSize:imageSize] autorelease];
+      [image addRepresentation:imageRep];
+
+      window.contentView.layer.contents = image;
+    }
+
     NSEvent *event;
     do {
-       event = [NSApp nextEventMatchingMask:NSEventMaskAny
-                                        untilDate:nil
-                                           inMode:NSDefaultRunLoopMode
-                                          dequeue:YES];
+      event = [NSApp nextEventMatchingMask:NSEventMaskAny
+                                 untilDate:nil
+                                    inMode:NSDefaultRunLoopMode
+                                   dequeue:YES];
 
       switch ([event type]) {
       default:
-        [NSApp sendEvent: event];
+        [NSApp sendEvent:event];
       }
     } while (event != nil);
   }
